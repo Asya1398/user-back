5e0485ea274f72db15b56842d91f8081d0a95db3
Asya1398 <asi.hakobyan@gmail.com>
2021-11-12 01:10:34 -0800

    added put router fro updating post

--- routes/post.js
+++ routes/post.js
@@ -6,7 +6,7 @@ const { verifyAuth } = require('../middleware');
 
 router.get('/', verifyAuth, posts.getPosts);
 router.post('/create', verifyAuth, posts.createPosts);
-router.post('/update', verifyAuth, posts.updatePosts);
+router.put('/update', verifyAuth, posts.updatePosts);
 router.post('/delete/:id', verifyAuth, posts.deletePosts);
 
 module.exports = router;
